e95d184bc5e50e6c6000e70a1fcb866de468865d
Usama Mehmood <usamamehmood717@gmail.com>
2018-05-20 18:34:51 +0500

    Started Home page

--- store/views.py
+++ store/views.py
@@ -1,3 +1,10 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
-# Create your views here.
+class Home(TemplateView):
+    template_name = 'store/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        context['page_title'] = 'Home'
+        return context
