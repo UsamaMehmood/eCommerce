1a9905067191d8fceb33772524cc5a7062d2ed74
Usama Mehmood <usamamehmood717@gmail.com>
2018-07-12 09:00:02 +0500

    Made an api

--- store/views.py
+++ store/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import TemplateView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 from django.shortcuts import render
+
 
 class Home(TemplateView):
     template_name = 'store/home.html'
@@ -8,3 +12,13 @@ class Home(TemplateView):
         context = super(Home, self).get_context_data(**kwargs)
         context['page_title'] = 'Home'
         return context
+
+
+class DummyApi(APIView):
+    def get(self, request):
+        name = request.GET.get('name', '')
+        response = {
+            'name': '{}'.format(name),
+            'message': 'Hello there! How are you?'
+        }
+        return Response(response, status=status.HTTP_200_OK)
